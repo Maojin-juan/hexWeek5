c47f162a437f260009a0a245b85149d3079cd9e9
Maojin-juan <edwardelric199887@gmail.com>
2024-08-02 16:37:04 +0800

    更新顏色及hover效果

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,15 @@ export default {
         ],
       },
 
-      colors: {},
+      colors: {
+        "gray-100": "#F8F9FA",
+        "gray-400": "#CED4DA",
+        "gray-600": "#6C757D",
+        "gray-900": "#212529",
+        "green-600": "#1DA365",
+        "green-700": "#198754",
+        "green-900": "#147046",
+      },
 
       fontFamily: {
         sans: ["Lato", "Roboto Mono", "sans-serif"],
